a6d4156d4083d93c796e74e56454f05e7dae20db
Vladimir Romanov <vladimirromanov@me.com>
2025-12-21 15:04:07 +0300

    start orm

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item(models):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
