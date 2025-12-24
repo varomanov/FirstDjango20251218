daacee8e75e2de371a67c8bcb745b8db0bca64e1
Vladimir Romanov <vladimirromanov@me.com>
2025-12-24 09:15:40 +0300

    add descriptions

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=300, default='Описание элемента')
 
     def __repr__(self):
-        return f"Item({self.name})"
+        return f"Item(id:{self.pk}, name:{self.name})"
--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 from django.db.models import Sum
-import pprint
+from django.core.exceptions import ObjectDoesNotExist
 
 USER = {
     "first_name": "Vladimir",
@@ -11,14 +11,6 @@ USER = {
     "phone": "888-000-99988",
     "email": "888@yandex.ru",
 }
-
-ITEMS = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 7, "name": "Картофель фри", "quantity": 0},
-    {"id": 8, "name": "Кепка", "quantity": 124},
-]
 
 menu = [
     {'name': 'Главная', 'path': '/'},
@@ -54,8 +46,10 @@ def items(request):
 
 def item(request, id: int):
     context = {'name': 'Товар не найден', 'quantity': 0, 'menu': menu}
-    products = Item.objects.filter(id=id)[0]
-    print(products.id, type(products))
-    if products:
-        return render(request, 'item.html', {'name': products.name, 'quantity': products.count, 'menu': menu})
-    return render(request, 'item.html', context)
+    try:
+        products = Item.objects.get(id=id)
+        if products:
+            return render(request, 'item.html', {'name': products.name, 'quantity': products.count, 'menu': menu, 'description': products.description})
+    except Item.DoesNotExist:
+        return render(request, 'item.html', context)
+    
